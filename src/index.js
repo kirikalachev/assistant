cac573a74b1ba81a5c5e311dc833979e0d0a6be9
kirikalachev <kalachev.k.2507@gmail.com>
2024-10-20 13:59:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 const router = createBrowserRouter( [
 
   {
-    path: '/',
+    path: '/assistant',
     element: <App />
   },
 
